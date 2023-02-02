e3e3c544fc95bf7a98d856739549f5727a491a9f
Gabriel Sene <sene300@gmail.com>
2023-02-02 13:11:28 -0300

    feat: Adicionando Pool de conexões

--- src/main/java/br/com/album/model/ConnectionFactory.java
+++ src/main/java/br/com/album/model/ConnectionFactory.java
@@ -4,10 +4,31 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 public class ConnectionFactory {
 	
+	DataSource dataSource;
+	
+	public ConnectionFactory() {
+		ComboPooledDataSource comboPooledDataSource  = new ComboPooledDataSource();
+		comboPooledDataSource.setJdbcUrl("jdbc:mysql://localhost:3306/albumcup?useTimezone=true&serverTimezone=UTC");
+		comboPooledDataSource.setUser("root");
+		comboPooledDataSource.setPassword("root");
+		comboPooledDataSource.setMaxPoolSize(30);
+		
+		this.dataSource=comboPooledDataSource;
+	}
+	
+	
+	
+	
+	
 	public Connection iniciaConexao() throws SQLException {
-		return DriverManager.getConnection("jdbc:mysql://localhost:3306/albumcup?useTimezone=true&serverTimezone=UTC", "root", "root");
+		
+		return this.dataSource.getConnection();
 	}
 
 }
